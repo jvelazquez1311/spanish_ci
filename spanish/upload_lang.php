3ecdbdcd08eae02b28428dace42e0682fd7ffe26
Alejandro Iván <alejandroivan@local.iosdevel.cl>
2013-10-28 03:00:06 -0300

    Correciones y Estandarización
    - Corrección de errores de ortografía y redacción, de acuerdo a normas
    actuales de la RAE.
    - Estandarización del español utilizado. Todas las oraciones ahora
    están escritas en español formal.
    - Algunos formatos fueron actualizados para coincidir más fielmente a
    los originales en inglés.

--- spanish/upload_lang.php
+++ spanish/upload_lang.php
@@ -1,19 +1,22 @@
 <?php
-$lang['upload_userfile_not_set'] = "No se puede encontrar una variable post llamado userfile.";
-$lang['upload_file_exceeds_limit'] = "El archivo subido excede el tamaño máximo permitido en el archivo de configuración de PHP.";
-$lang['upload_file_exceeds_form_limit'] = "El archivo subido excede el tamaño máximo permitido por el formulario de envío.";
-$lang['upload_file_partial'] = "El archivo fue parcialmente cargado.";
-$lang['upload_no_temp_directory'] = "La carpeta temporal no se encuentra.";
-$lang['upload_unable_to_write_file'] = "El archivo no pudo ser escrito en el disco.";
-$lang['upload_stopped_by_extension'] = "El archivo de carga fue detenido por la extensión.";
-$lang['upload_no_file_selected'] = "Usted no ha seleccionado un archivo para cargar.";
+
+$lang['upload_userfile_not_set'] = "No se puede encontrar una variable POST llamada userfile.";
+$lang['upload_file_exceeds_limit'] = "El archivo cargado excede el tamaño máximo permitido en su archivo de configuración de PHP.";
+$lang['upload_file_exceeds_form_limit'] = "El archivo cargado excede el tamaño máximo permitido por el formulario de envío.";
+$lang['upload_file_partial'] = "El archivo solo fue parcialmente cargado.";
+$lang['upload_no_temp_directory'] = "No se encuentra el directorio temporal.";
+$lang['upload_unable_to_write_file'] = "El archivo no se pudo escribir al disco.";
+$lang['upload_stopped_by_extension'] = "La carga del archivo fue detenida por una extensión.";
+$lang['upload_no_file_selected'] = "No ha seleccionado un archivo para cargar.";
 $lang['upload_invalid_filetype'] = "El tipo de archivo que está intentando cargar no está permitido.";
-$lang['upload_invalid_filesize'] = "El archivo que está intentando cargar es mayor que el tamaño permitido.";
-$lang['upload_invalid_dimensions'] = "La imagen que está intentando cargar supera las máxima de altura o anchura.";
-$lang['upload_destination_error'] = "Se encontró un problema al intentar mover el archivo subido hasta el destino final.";
-$lang['upload_no_filepath'] = "El camino de subida no parece ser válido.";
-$lang['upload_no_file_types'] = "No se ha especificado ningún tipo de archivo permitido.";
+$lang['upload_invalid_filesize'] = "El archivo que está intentando cargar es más grande que el tamaño permitido.";
+$lang['upload_invalid_dimensions'] = "La imagen que está intentando cargar supera la altura o el ancho máximos.";
+$lang['upload_destination_error'] = "Se encontró un problema mientras se intentaba mover el archivo cargado hacia su destino final.";
+$lang['upload_no_filepath'] = "La ruta de carga no parece ser válida.";
+$lang['upload_no_file_types'] = "No ha especificado ningún tipo de archivo permitido.";
 $lang['upload_bad_filename'] = "El nombre del archivo que ha enviado ya existe en el servidor.";
-$lang['upload_not_writable'] = "La carpeta de destino de carga no parece tener permisos de escritura.";
+$lang['upload_not_writable'] = "La carpeta de destino de la carga no parece poder escribirse.";
+
+
 /* End of file upload_lang.php */
-/* Location: ./system/language/spanish/upload_lang.php */
+/* Location: ./system/language/english/upload_lang.php */
